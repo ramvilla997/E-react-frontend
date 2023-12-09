973bad2490b1bd908ac52777cfb2ede77c657645
AnuTuse <atuse066@uottawa.ca>
2023-12-08 20:40:01 -0500

    Profile Page - Anushka

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -37,48 +37,37 @@ const ProfilePage = () => {
   // Filter the user data based on loginData.id
   const filteredUserData = userData.filter((user) => user.id === loginData.id);
 
+  const renderUserInfo = (label, value) => (
+    <div key={label} className="user-info-item">
+      <strong>{label}:</strong> {value}
+    </div>
+  );
+
   return (
     <div className="profile-page">
       <h2>Patient Profile Details</h2>
-      <div className="table-container">
-        <table className="user-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>First Name</th>
-              <th>Middle Name</th>
-              <th>Last Name</th>
-              <th>Age</th>
-              <th>Blood Group</th>
-              <th>Height</th>
-              <th>Weight</th>
-              <th>Race</th>
-              <th>Mobile Number</th>
-              <th>Email</th>
-              <th>Address</th>
-              <th>Location</th>
-              <th>City</th>
-              <th>Province</th>
-              <th>Postal Code</th>
-              <th>Latitude</th>
-              <th>Longitude</th>
-              <th>Health Card Number</th>
-              <th>Passport Number</th>
-              <th>PR Number</th>
-              <th>Driver's Licence Number</th>
-              <th>UUID</th>
-              <th>Password</th>
-              <th>Verification</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredUserData.map((user) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>                <td>{user.FName}</td>                <td>{user.MName}</td>                <td>{user.LName}</td>                <td>{user.Age}</td>                <td>{user.BloodGroup}</td>                <td>{user.height}</td>                <td>{user.weight}</td>                <td>{user.race}</td>                <td>{user.MobileNumber}</td>                <td>{user.EmailId}</td>                <td>{user.Address}</td>                <td>{user.Location}</td>                <td>{user.City}</td>                <td>{user.Province}</td>                <td>{user.PostalCode}</td>                <td>{user.Latitude}</td>                <td>{user.Longitude}</td>                <td>{user.HCardNumber}</td>                <td>{user.PassportNumber}</td>                <td>{user.PRNumber}</td>                <td>{user.DLNumber}</td>                <td>{user.uuid}</td>                <td>{user.password}</td>                <td>{user.verification}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="user-info-container">
+        {filteredUserData.map((user) => (
+          <div key={user.id} className="user-info">
+            <div className="column">
+              <h3>Personal Information</h3>
+              {renderUserInfo('First Name', user.FName)}
+              {renderUserInfo('Middle Name', user.MName)}
+              {renderUserInfo('Last Name', user.LName)}
+              {renderUserInfo('Address', user.Address)}
+              {renderUserInfo('Phone', user.MobileNumber)}
+              {renderUserInfo('Email', user.EmailId)}
+            </div>
+            <div className="column">
+              <h3>Demographics</h3>
+              {renderUserInfo('Age', user.Age)}
+              {renderUserInfo('Gender', user.Gender)}
+              {renderUserInfo('Blood Type', user.BloodGroup)}
+              {renderUserInfo('Height', `${user.height} cm`)}
+              {renderUserInfo('Weight', `${user.weight} kg`)}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
